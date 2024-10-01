696c94ff46993e24ecf7045aceb3edf2e1fb6735
Niklas-Sommer <niklas.sommer03@gmail.com>
2024-10-01 20:42:31 +0200

    The plugin now searches for the newest file in the images folder and inserts it at the current position

--- main.ts
+++ main.ts
@@ -1,4 +1,4 @@
-import { App, Editor, MarkdownView, Modal, Notice, Plugin, PluginSettingTab, Setting } from 'obsidian';
+import { App, MarkdownView, Plugin, PluginSettingTab, Setting, TAbstractFile, TFile } from 'obsidian';
 
 // Remember to rename these classes and interfaces!
 
@@ -7,7 +7,7 @@ interface MyPluginSettings {
 }
 
 const DEFAULT_SETTINGS: MyPluginSettings = {
-	mySetting: 'default'
+	mySetting: 'Images'
 }
 
 export default class MyPlugin extends Plugin {
@@ -16,53 +16,41 @@ export default class MyPlugin extends Plugin {
 	async onload() {
 		await this.loadSettings();
 
-		// This creates an icon in the left ribbon.
-		const ribbonIconEl = this.addRibbonIcon('dice', 'Sample Plugin', (evt: MouseEvent) => {
-			// Called when the user clicks the icon.
-			new Notice('This is a notice!');
-		});
-		// Perform additional things with the ribbon
-		ribbonIconEl.addClass('my-plugin-ribbon-class');
-
-		// This adds a status bar item to the bottom of the app. Does not work on mobile apps.
-		const statusBarItemEl = this.addStatusBarItem();
-		statusBarItemEl.setText('Status Bar Text');
-
-		// This adds a simple command that can be triggered anywhere
-		this.addCommand({
-			id: 'open-sample-modal-simple',
-			name: 'Open sample modal (simple)',
-			callback: () => {
-				new SampleModal(this.app).open();
-			}
-		});
 		// This adds an editor command that can perform some operation on the current editor instance
 		this.addCommand({
-			id: 'sample-editor-command',
-			name: 'Sample editor command',
-			editorCallback: (editor: Editor, view: MarkdownView) => {
-				console.log(editor.getSelection());
-				editor.replaceSelection('Sample Editor Command');
-			}
-		});
-		// This adds a complex command that can check whether the current state of the app allows execution of the command
-		this.addCommand({
-			id: 'open-sample-modal-complex',
-			name: 'Open sample modal (complex)',
-			checkCallback: (checking: boolean) => {
-				// Conditions to check
+			id: 'insert-latest-image-command',
+			name: 'Insert Latest Image',
+			editorCheckCallback: (checking, editor, ctx) => {
 				const markdownView = this.app.workspace.getActiveViewOfType(MarkdownView);
 				if (markdownView) {
-					// If checking is true, we're simply "checking" if the command can be run.
-					// If checking is false, then we want to actually perform the operation.
 					if (!checking) {
-						new SampleModal(this.app).open();
+						const folder = this.app.vault.getFolderByPath(this.settings.mySetting);
+						if (folder) {
+							let file: any = null;
+							let maxTime = 0;
+							folder.children.every((value: TAbstractFile, index: number, array: TAbstractFile[]) => {
+								if (value instanceof TFile) {
+									if (value.stat.ctime > maxTime) {
+										file = value;
+										maxTime = value.stat.ctime;
+									}
+								}
+
+								return true;
+							});
+
+							if (file instanceof TFile) {
+								editor.replaceSelection(this.app.fileManager.generateMarkdownLink(file, file.path) + "\n");
+								const cursor = editor.getCursor();
+								cursor.line += 1;
+								editor.setCursor(cursor);
+							}
+						}
 					}
 
-					// This command will only show up in Command Palette when the check function returns true
 					return true;
 				}
-			}
+			},
 		});
 
 		// This adds a settings tab so the user can configure various aspects of the plugin
@@ -91,22 +79,6 @@ export default class MyPlugin extends Plugin {
 	}
 }
 
-class SampleModal extends Modal {
-	constructor(app: App) {
-		super(app);
-	}
-
-	onOpen() {
-		const {contentEl} = this;
-		contentEl.setText('Woah!');
-	}
-
-	onClose() {
-		const {contentEl} = this;
-		contentEl.empty();
-	}
-}
-
 class SampleSettingTab extends PluginSettingTab {
 	plugin: MyPlugin;
 
@@ -116,18 +88,18 @@ class SampleSettingTab extends PluginSettingTab {
 	}
 
 	display(): void {
-		const {containerEl} = this;
+		const { containerEl } = this;
 
 		containerEl.empty();
 
 		new Setting(containerEl)
-			.setName('Setting #1')
-			.setDesc('It\'s a secret')
+			.setName('Image Folger')
+			.setDesc('From this folder the last image will be inserted at the current cursor position.')
 			.addText(text => text
-				.setPlaceholder('Enter your secret')
+				.setPlaceholder('Images')
 				.setValue(this.plugin.settings.mySetting)
 				.onChange(async (value) => {
-					this.plugin.settings.mySetting = value;
+					this.plugin.settings.mySetting = value.replace(/^\/+|\/+$/g, '');
 					await this.plugin.saveSettings();
 				}));
 	}
